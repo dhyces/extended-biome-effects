c338f6d09855992b0d56481fe2fb62910aaab413
dhyces <pokmon987@gmail.com>
2023-07-26 01:09:04 -0400

    move datagen to test

--- testmod/src/main/java/dev/dhyces/testmod/DatagenTest.java
+++ testmod/src/main/java/dev/dhyces/testmod/DatagenTest.java
@@ -1,11 +1,14 @@
-package dev.dhyces.biomeextensions;
+package dev.dhyces.testmod;
 
+import dev.dhyces.biomeextensions.ApiAccess;
+import dev.dhyces.biomeextensions.RegistryHelper;
 import dev.dhyces.biomeextensions.extension.BiomeExtension;
 import dev.dhyces.biomeextensions.extension.effects.FogExtension;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -14,8 +17,8 @@ import net.minecraftforge.fml.common.Mod;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
-@Mod.EventBusSubscriber(modid = ExtendedBiomeEffects.MODID)
-public class Datagen {
+@Mod.EventBusSubscriber(modid = "testmod", bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DatagenTest {
 
     @SubscribeEvent
     static void setupDatagen(final GatherDataEvent event) {
@@ -25,9 +28,9 @@ public class Datagen {
 
         RegistrySetBuilder builder = new RegistrySetBuilder()
                 .add(ApiAccess.EXTENSION_REGISTRY_KEY, pContext -> {
-                    pContext.register(RegistryHelper.registryKey(ExtendedBiomeEffects.id("plains")), BiomeExtension.single(new FogExtension(4, 0.3f)));
+                    pContext.register(RegistryHelper.registryKey(new ResourceLocation("nether_wastes")), BiomeExtension.single(new FogExtension(0, 999, 999f)));
                 });
 
-        generator.addProvider(event.includeServer(), new DatapackBuiltinEntriesProvider(packOutput, lookupProvider, builder, Set.of(ExtendedBiomeEffects.MODID)));
+        generator.addProvider(event.includeServer(), new DatapackBuiltinEntriesProvider(packOutput, lookupProvider, builder, Set.of("minecraft")));
     }
 }
